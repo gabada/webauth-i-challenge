d79f0ac82b5f021d197af94719226bba2529e564
Gill Abada <gill@callifi.com>
2019-04-01 14:04:24 -0400

    add endpoint to GET users.

---
+++ users/users-router.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const Users = require('./users-model.js');
+const bcrypt = require('bcrypt');
+
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: 'You shall not pass!' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  } else {
+    res.status(401).json({ message: 'Please provide credentials' });
+  }
+}
+
+router.get('/', restricted, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+module.exports = router;
